0fa62ff15b3447994d1f0684e27a06e25d87afe1
lgcjh0s <redballs@naver.com>
2023-10-12 19:13:17 +0900

    c

---
+++ public/js/app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsx_runtime_1 = require("react/jsx-runtime");
+var App = function () { return ((0, jsx_runtime_1.jsx)("div", { children: "Hello World" })); };
+exports.default = App;
